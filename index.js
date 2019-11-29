070a143728d5039480ac4d4b7beb0090f579101b
Luka Perisic <luka.perisic@gauss.hr>
2019-11-29 11:58:35 +0100

    starting tests

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 const test = 'Neki test';
 console.log(test);
+
+let syntaxAlphabet = 'abcdefghijklmnopqrstuvwxyz#'.toUpperCase().split('');
+
+let allowedSyntaxWords = ['LOAD'];
+
+let syntaxMatrix = [];
+let c, r;
+for (let i = 0; i < allowedSyntaxWords.length; i++) {
+    for (let j = 0; j < syntaxAlphabet.length; j++) {
+        if (!syntaxMatrix[i]) {
+            syntaxMatrix[i] = {};
+        }
+        // syntaxMatrix[i]
+        // syntaxMatrix[i][`${r}`] = syntaxAlphabet[j];
+    }
+}
+console.log(syntaxMatrix);
+let word, lastLetter;
+for(let i = 0; i < allowedSyntaxWords.length; i++) {
+    word = allowedSyntaxWords[i];
+    [...word].forEach(
+        c => {
+            if (lastLetter) {
+                    syntaxMatrix[i][`${lastLetter}`] = c;
+            }
+            lastLetter = c;
+            console.log(lastLetter);
+        }
+
+    )
+}
+console.log(syntaxMatrix);
