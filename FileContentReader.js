dddf3e2cf79bd6603f7540d2f1fc246c70c5e1db
Luka Perisic <luka.perisic@gauss.hr>
2019-11-29 12:30:47 +0100

    finished

--- FileContentReader.js
+++ FileContentReader.js
@@ -2,13 +2,15 @@ var fs  = require("fs")
 var newLineArray = fs.readFileSync('./text.txt').toString().split('\n')
 var spaceArray = []
 for (i = 0; i < newLineArray.length; i++) {
-    spaceArray.push(newLineArray[i].split(' '))
+    spaceArray.push(newLineArray[i].replace('\r','').split(' '))
 }
 var characterArray = []
 for (i = 0; i<spaceArray.length; i++) {
     var item = spaceArray[i]
     for (j = 0; j<item.length; j++) {
-        characterArray.push(item[j])
+        if (item[j] !== '') {
+            characterArray.push(item[j])
+        }
     }
 }
 
--- index.js
+++ index.js
@@ -1,36 +1,66 @@
-const asd = require('./FileContentReader')
+const wordsProvider = require('./FileContentReader')
 const test = 'Neki test';
 
-console.log(asd.charArray());
+let testWords = wordsProvider.charArray();
 
 let syntaxAlphabet = 'abcdefghijklmnopqrstuvwxyz#'.toUpperCase().split('');
 
-let allowedSyntaxWords = ['LOAD'];
+let allowedSyntaxWords = ['LOAD', 'ADD', 'SUB', 'DIV', 'BEGIN', 'END', 'R0', 'R1',
+    'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9'];
 
 let syntaxMatrix = [];
-let c, r;
 for (let i = 0; i < allowedSyntaxWords.length; i++) {
-    for (let j = 0; j < syntaxAlphabet.length; j++) {
-        if (!syntaxMatrix[i]) {
-            syntaxMatrix[i] = {};
-        }
-        // syntaxMatrix[i]
-        // syntaxMatrix[i][`${r}`] = syntaxAlphabet[j];
+    if (!syntaxMatrix[i]) {
+        syntaxMatrix[i] = {};
     }
 }
 console.log(syntaxMatrix);
 let word, lastLetter;
-for(let i = 0; i < allowedSyntaxWords.length; i++) {
+for (let i = 0; i < allowedSyntaxWords.length; i++) {
     word = allowedSyntaxWords[i];
+    lastLetter = '';
     [...word].forEach(
         c => {
             if (lastLetter) {
-                    syntaxMatrix[i][`${lastLetter}`] = c;
+                syntaxMatrix[i][`${lastLetter}`] = c;
             }
             lastLetter = c;
             console.log(lastLetter);
         }
-
     )
 }
 console.log(syntaxMatrix);
+let generatedSyntaxMatrix = [];
+for (let i = 0; i < testWords.length; i++) {
+    if (!generatedSyntaxMatrix[i]) {
+        generatedSyntaxMatrix[i] = {};
+    }
+}
+for (let i = 0; i < testWords.length; i++) {
+    let word = testWords[i], lastLetter = '';
+    [...word].forEach(
+        c => {
+            if (lastLetter) {
+                generatedSyntaxMatrix[i][`${lastLetter}`] = c;
+            }
+            lastLetter = c;
+            console.log(lastLetter);
+        }
+    )
+}
+console.log(generatedSyntaxMatrix);
+
+let counter = 0;
+generatedSyntaxMatrix.forEach(
+    genWord => {
+        syntaxMatrix.forEach(
+            syntaxWord => {
+                if(JSON.stringify(syntaxWord) === JSON.stringify(genWord)) {
+                    counter ++;
+                }
+            }
+        );
+    }
+);
+
+console.log(`UKUPNO: ${testWords.length}, VALIDNO: ${counter}`);
